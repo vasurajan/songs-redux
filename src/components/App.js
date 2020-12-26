c2216d1d1b4f0cdeb76ebaa37ea920e555c6bac3
vasurajan <63305945+vasurajan@users.noreply.github.com>
2020-12-27 02:39:53 +0530

    done

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,19 @@
 import React from 'react';
 
-function App() {
+import SongList from './SongList';
+import SongDetail from './SongDetail';
+
+const App = () => {
   return (
-    <div className="App">
-      App
+    <div className="ui container grid">
+      <div className="ui row">
+        <div className="column eight wide">
+          <SongList />
+        </div>
+        <div className="column eight wide">
+          <SongDetail />
+        </div>
+      </div>
     </div>
   );
 }
